d84604dbb783f5d55277aa30893c923f37288aaa
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-10-01 17:32:40 -0500

    cambios 2.o

--- src/pages/edit_estrato.js
+++ src/pages/edit_estrato.js
@@ -1,14 +1,63 @@
 import React from "react";
 import "../styles/styles.css";
+import Swal from "sweetalert2";
 class Editar_estrato extends React.Component {
-render(){
-  return(
-    <h1> si ven esto es por que stiven aun no lo programa</h1>
+  _Alerta() {
+    Swal.fire(
+      "Estrato eliminado con exito",
+      "Haz clink en el boton",
+      "success"
+    );
+  }
+  render() {
+    return (
+      <div className="container-fluid fondo">
+        <div className="row">
+          <div className="col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+          <div className="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+            <div className="row">
+              <div className=" col-lg-12">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Código</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  <small id="emailHelp" class="form-text text-muted"></small>
+                  <label for="exampleInputEmail1">Nombre</label>
+                  <br />
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
 
-  );
+                <div className="col text-center">
+                  <button
+                    onClick={this._Alerta}
+                    type="button"
+                    className="btn btn-outline-dark boton"
+                  >
+                    <a href="/estrato">Guardar</a>
+                  </button>
+                  <br/>
+                  <br/>
+                  <button type="button" className="btn btn-outline-dark boton">
+                    <a href="/estrato">Regresar</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-}
-
 
 export default Editar_estrato;
