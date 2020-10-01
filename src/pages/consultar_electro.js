4fdd5ffef5cdef0021eeb4e35a7f0e36ec52e5e6
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-10-01 14:13:14 -0500

    consulta electro

---
+++ src/pages/consultar_electro.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../styles/styles.css";
+import Swal from "sweetalert2";
+class Electro extends React.Component {
+  _Alerta() {
+    Swal.fire(
+      "Electrodomestico eliminado con exito",
+      "Haz clink en el boton",
+      "success"
+    );
+  }
+  render() {
+    return (
+      <div className="container-fluid fondo">
+        <div className="row">
+          <div className="col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+          <div className="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+            <div className="row">
+              <div className=" col-lg-12">
+                <div className="form-group">
+                  <div className="col text-center">
+                  
+                  <h1> Buscar electrodomestico</h1>
+                  <br/>
+                  <br/>
+                  
+                  <nav className="navbar ">
+                    <form className="form-inline">
+                      <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Buscar consumo"
+                        aria-label="Search"
+                      />
+                      <button
+                        className="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                      >
+                        Buscar
+                      </button>
+                    </form>
+                  </nav>
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Electro;
