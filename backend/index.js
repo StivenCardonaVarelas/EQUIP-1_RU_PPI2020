883bb3c97d3661ed03db3418b87bdfa1ce5392e5
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-11-10 17:20:41 -0500

    agregar index

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,26 @@
-const express=require('express');
+const express = require('express');
 const app=express();
-const usuario=require('./routes/usuario');
-//const estrato=require('./routes/estrato');
-//const electrodomestico=require('./routes/electrodomestico');
-//const costo_estrato=require('./routes/costos_estrato');
-const costo_mensual=require('./routes/costos_mensual');
-// Ajustess 
-// Ajustess 
-app.set('port', 3000);//puerto único  //firewall
+//const routes=require('./routes/routes');
+//const modulos=require('./routes/modulos');
+const estrato=require('./routes/estrato');
+const Cestrato=require('./routes/costo_Estrato');
 
-// middleware
+
+
+//ajustes
+app.set('port', 3000); //puerto unico //firewall
+
+
+//middleware
 app.use(express.json());
-// ajustess
-// defino middleware
-app.use('/api',usuario);
-app.use('/api/costo_mensual',costo_mensual);
 
-//app.use('/api/modulos',modulos);
-//app.use('/api/colegios',colegios);
+//ajustes
 
+app.use('/api', estrato);
+app.use('/api', Cestrato);
+//app.use('/api/modulos', modulos);
+//app.use('/api/colegios', colegios)
 
-app.listen(app.get('port'),()=>{
-    console.log('listening on port' + app.get('port'));
+app.listen(app.get('port'), ()=>{
+    console.log('listening on port'+ app.get('port'));
 });
