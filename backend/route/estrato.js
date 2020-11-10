56eaa345b8fcd902dc1f3c409a23b9a04c8c45ef
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-11-10 17:15:10 -0500

    Estrato

---
+++ backend/route/estrato.js
@@ -0,0 +1,28 @@
+const express=require('express');//tabnine
+const router=express.Router();
+const mysqlConnection =require('../db/db');
+//colocar middleware
+router.get('/estrato', (req, res) => {
+    //callbacks
+    mysqlConnection.query('SELECT * FROM Estrato', (err, rows, fields) => {
+        //si no hay error
+        if (!err) {
+            //si es verdadero
+            res.json(rows);
+            //si no es verdadero
+        } else {
+            console.log(err);
+
+        }
+
+
+
+    })//fin query 
+})//fin del get
+
+
+
+
+
+
+module.exports=router;
