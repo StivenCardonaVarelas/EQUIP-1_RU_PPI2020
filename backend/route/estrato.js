ddbcd68720a70dc2c0881ad2409d09ffcb0fa947
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-11-10 20:29:49 -0500

    estrato

--- backend/route/estrato.js
+++ backend/route/estrato.js
@@ -21,6 +21,35 @@ router.get('/estrato', (req, res) => {
 })//fin del get
 
 
+//agregar un nuevo estrato
+router.post('/nuevo-estrato', (req, res) => {
+    const { codigo, nombre} = req.body;
+    let estrato = [codigo, nombre]
+
+    let nuevoEstrato = `INSERT INTO Estrato(codigo, nombre) VALUES (?,?)`;
+    mysqlConnection.query(nuevoEstrato, estrato, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        } else {
+            res.json({ message: ' estrato creado satisfactoriamente' })
+        }
+    })//finquery
+})//finpost
+
+//actualizar un estrato
+router.put('/estrato', (req, res) => {
+    const { nombre } = req.body;
+    const { codigo } = req.params;
+    mysqlConnection.query(`UPDATE Estrato SET codigo=?,nombre=? WHERE codigo=?`,
+        [ nombre, codigo], (err, rows, fields) => {
+            if (!err) {
+
+                res.json({ status: `Estrato Actualizado exitosamente` });
+            } else {
+                console.log(err);
+            }
+        })
+});//fin actua estrato
 
 
 
