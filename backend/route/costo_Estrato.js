d43ee03890e3db93f2de44f8de1cbe9f59a0b4bb
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-11-10 20:28:32 -0500

    costo_estrato

--- backend/route/costo_Estrato.js
+++ backend/route/costo_Estrato.js
@@ -22,6 +22,35 @@ router.get('/costo_Estrato', (req, res) => {
 
 
 
+//agregar un nuevo costo_Estrato
+router.post('/nuevo-cestrato', (req, res) => {
+    const { costo, estrato} = req.body;
+    let costoE = [costo, estrato]
+
+    let nuevocostoE = `INSERT INTO costo_Estrato(costo, estrato) VALUES (?,?)`;
+    mysqlConnection.query(nuevocostoE, costoE, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        } else {
+            res.json({ message: ' costo_estrato creado satisfactoriamente' })
+        }
+    })//finquery
+})//finpost
+
+//actualizar un costo_estrato
+router.put('/costo_estrato', (req, res) => {
+    const { costo } = req.body;
+    const { estrato } = req.params;
+    mysqlConnection.query(`UPDATE costo_Estrato SET costo=?,estrato=? WHERE costo=?`,
+        [ costo, estrato], (err, rows, fields) => {
+            if (!err) {
+
+                res.json({ status: `costo_Estrato Actualizado exitosamente` });
+            } else {
+                console.log(err);
+            }
+        })
+});//fin actua estrato
 
 
 
