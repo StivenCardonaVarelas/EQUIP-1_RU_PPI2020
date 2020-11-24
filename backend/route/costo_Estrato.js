0d1dc98e61b5d7855491c916283a1635da12beb8
danielmarquezunico <66494522+danielmarquezunico@users.noreply.github.com>
2020-11-24 17:16:12 -0500

    costo_Estrato.js

---
+++ backend/route/costo_Estrato.js
@@ -0,0 +1,101 @@
+const express=require('express');//tabnine
+const router=express.Router();
+const mysqlConnection =require('../db/db');
+
+
+
+
+
+//colocar middleware
+router.get('/costo_Estrato', (req, res) => {
+    //callbacks
+    mysqlConnection.query('SELECT * FROM costo_Estrato', (err, rows, fields) => {
+        //si no hay error
+        if (!err) {
+            //si es verdadero
+            res.json(rows);
+            //si no es verdadero
+        } else {
+            console.log(err);
+
+        }
+
+
+
+    })//fin query 
+})//fin del get
+
+
+//bucar
+router.get('/costo_Estrato/:Nombre',(req,res)=>{
+    const {Nombre} = req.params; //DATO ESTRATO númerico entero
+    mysqlConnection.query('SELECT * FROM costo_Estrato WHERE Nombre =?', [Nombre],(err,rows,fields)=>{
+        if(!err){
+            res.json(rows[0])
+        }else{
+            console.log(err);
+        }
+    })
+}) // fin buscar
+
+
+
+
+
+
+
+
+
+
+//agregar un nuevo costo_Estrato
+router.post('/nuevo-costo_estrato', (req, res) => {
+    const { Idcodigo, Costo, Estrato} = req.body;
+    let costoE = [ Idcodigo, Costo, Estrato]
+
+    let nuevocostoE = `INSERT INTO costo_Estrato(Idcodigo, Costo, Estrato) VALUES (?,?,?)`;
+    mysqlConnection.query(nuevocostoE, costoE, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        } else {
+            res.json({ message: ' costo_estrato creado satisfactoriamente' + Costo})
+        }
+    })//finquery
+})//finpost
+
+//actualizar un costo_estrato
+router.put('/costo_estrato', (req, res) => {
+    const { Costo } = req.body;
+    const { Estrato } = req.params;
+    mysqlConnection.query(`UPDATE costo_Estrato SET Costo=?,Estrato=? WHERE Costo=?`,
+        [ Costo, Estrato], (err, rows, fields) => {
+            if (!err) {
+
+                res.json({ status: `costo_Estrato Actualizado exitosamente` });
+            } else {
+                console.log(err);
+            }
+        })
+});//fin actua estrato
+
+//ESTE ES ELIMINAR COSTO_ESTRATO
+
+router.delete('/costo_Estrato/:Idcodigo',(req,res)=>{
+    const {Idcodigo} = req.params.Idcodigo;
+
+    if(!Idcodigo){
+        res.status(401).json({error: "Debes especificar el estrato del costo por estrato que deseas eliminar"});
+    } else {
+        const indexCosto_estrato = Costo_estrato.findIndex((Costo_estrato)=> Costo_estrato.Estrato ===Estrato);
+        Estrato.splice(indexCosto_estrato, 1);
+        const json_Costo_estrato= JSON.stringify(Costo_estrato);
+        fs.writeFileSync('./costo_Estrato.json',json_Costo_estrato,"utf-8");
+
+        res.status(200).json(Costo_estrato);
+    }
+});
+
+
+
+
+
+module.exports=router;
