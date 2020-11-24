59b1a827b0bf1f92f4d540ac69411c94a4bfcdf5
StivenCardonaVarelas <66332728+StivenCardonaVarelas@users.noreply.github.com>
2020-11-23 20:09:10 -0500

    post terminado (daniel)
    Falta el put a carga de carolina

--- backend/route/costo_mensual.js
+++ backend/route/costo_mensual.js
@@ -13,18 +13,24 @@ router.get('/costo_mensual', (req, res) => {
 })// fin
 //agregar un nuevo costo_mensual
 router.post('/nuevo-costo_mensual', (req, res) => {
-    const { electrodomestico, cantidad, potencia, horas_uso, consumo_diario, consumo_mensual} = req.body;
-    let costo_mensual = [electrodomestico, cantidad, potencia, horas_uso, consumo_diario, consumo_mensual]
- 
-    let nuevoCosto_mensual = `INSERT INTO consumo_Mensual(electrodomestico, cantidad, potencia, horas_uso, consumo_diario, consumo_mensual) VALUES (?,?,?,?,?,?)`;
+    const { IdcostoM, Electrodomestico, Cantidad, Potencia, Horas_uso, Consumo_diario, Consumo_mensual } = req.body;//1 Captura
+    let costo_mensual = [IdcostoM, Electrodomestico, Cantidad, Potencia, Horas_uso, Consumo_diario, Consumo_mensual];// Arreglo json
+    //Definir el scrip sql en una variable
+    let nuevoCosto_mensual = 'INSERT  INTO costo_Mensual(IdcostoM, Electrodomestico, Cantidad, Potencia, Horas_uso, Consumo_diario, Consumo_mensual) values (?,?,?,?,?,?,?)';
     mysqlConnection.query(nuevoCosto_mensual, costo_mensual, (err, results, fields) => {
+        //Si hay error
         if (err) {
+            //Verdadero
             return console.error(err.message);
-        } else {
-            res.json({ message: ' costo mensual creado satisfactoriamente' })
-        }
-    })//finquery
-})//finpost
+        } else {//Si no
+            //Falso
+            
+            res.json({ message: 'costo mensual creado '});
+        }//Fin Si
+    })
+})//Fin guardar un costo mensual
+
+
 //actualizar un costo_mensual
 router.put('/costo_mensual', (req, res) => {
     const {  cantidad, potencia, horas_uso, consumo_diario, consumo_mensual } = req.body;
